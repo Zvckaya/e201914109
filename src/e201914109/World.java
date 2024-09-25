6759b1d733ac5d809fa45538eadd588c8d00fd83
HEO NAMGYU <lzvckaya@gmail.com>
2024-09-25 09:16:45 +0900

    fifth

--- src/e201914109/World.java
+++ src/e201914109/World.java
@@ -4,5 +4,8 @@ public class World {
     public static void run() {
         System.out.println("201914109");
       }
+    public static void run2(){
+        System.err.println("hello world");
+    }
     
 }
